5aba89da027b05e4b23ecef3a6495ec5804e02c0
HP <illailanadiah@gmail.com>
2024-06-26 15:57:20 +0800

    gradle problem solved 26/6/2024

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,10 +58,11 @@ dependencies {
     // Firebase Authentication and Database
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-database-ktx")
+    implementation("com.google.firebase:firebase-database:21.0.0")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.0")
 }
 
 afterEvaluate {
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,16 @@
 buildscript {
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.4")
+        classpath("com.android.tools.build:gradle:8.5.0")
         classpath ("com.google.gms:google-services:4.3.10")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
     }
 }
 
 plugins {
-    id("com.android.application") version "8.1.4" apply false
-    id("com.android.library") version "8.1.4" apply false
+    id("com.android.application") version "8.5.0" apply false
+    id("com.android.library") version "8.5.0" apply false
     id("org.jetbrains.kotlin.jvm") version "1.8.10" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
