bd38cfac16a32c4f3137b502e24e61ea96e95f3b
HP <illailanadiah@gmail.com>
2024-07-14 10:25:55 +0800

    updated

--- app/src/main/java/com/example/pondokdarus/PrincipalReportActivity.java
+++ app/src/main/java/com/example/pondokdarus/PrincipalReportActivity.java
@@ -1,11 +1,63 @@
 package com.example.pondokdarus;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;public class PrincipalReportActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+public class PrincipalReportActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.principal_report); // Make sure this matches your XML file name
-    }
-}
+        setContentView(R.layout.principal_report);
+
+        Button btnGrade = findViewById(R.id.gradeBtn);
+        btnGrade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle grade button click
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                       .replace(R.id.gradeFragment, GradeFragment.class, null)
+                       .setReorderingAllowed(true)
+                       .addToBackStack("name")
+                       .commit();
+
+            }
+        });
+
+        Button btnMonth = findViewById(R.id.monthBtn);
+        btnMonth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle grade button click
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.monthFragment, MonthFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name")
+                        .commit();
+
+            }
+        });
+
+        Button btnYear = findViewById(R.id.yearBtn);
+        btnYear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle grade button click
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.yearFragment, YearFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name")
+                        .commit();
+
+            }
+        });
+
+    }}
