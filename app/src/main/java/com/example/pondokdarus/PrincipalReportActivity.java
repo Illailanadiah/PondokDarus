39de07dca8aac8bc8e2bce2acf920eaac44e4da2
HP <illailanadiah@gmail.com>
2024-07-15 01:20:48 +0800

    updated

--- app/src/main/java/com/example/pondokdarus/PrincipalReportActivity.java
+++ app/src/main/java/com/example/pondokdarus/PrincipalReportActivity.java
@@ -1,63 +1,59 @@
 package com.example.pondokdarus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class PrincipalReportActivity extends AppCompatActivity {
 
+    private ImageView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.principal_report);
 
-        Button btnGrade = findViewById(R.id.gradeBtn);
-        btnGrade.setOnClickListener(new View.OnClickListener() {
+
+        backButton = findViewById(R.id.back_icon);
+        setupFragmentButton(R.id.gradeBtn, GradeFragment.class, "gradeFragment");
+        setupFragmentButton(R.id.monthBtn, MonthFragment.class, "monthFragment");
+        setupFragmentButton(R.id.yearBtn, YearFragment.class, "yearFragment");
+
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Handle grade button click
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                fragmentManager.beginTransaction()
-                       .replace(R.id.gradeFragment, GradeFragment.class, null)
-                       .setReorderingAllowed(true)
-                       .addToBackStack("name")
-                       .commit();
-
+                Intent intent = new Intent(PrincipalReportActivity.this, PrincipalMainActivity.class);
             }
         });
+    }
 
-        Button btnMonth = findViewById(R.id.monthBtn);
-        btnMonth.setOnClickListener(new View.OnClickListener() {
+    private void setupFragmentButton(int buttonId, Class<? extends Fragment> fragmentClass, String backstackName) {
+        Button button = findViewById(buttonId);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Handle grade button click
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                fragmentManager.beginTransaction()
-                        .replace(R.id.monthFragment, MonthFragment.class, null)
-                        .setReorderingAllowed(true)
-                        .addToBackStack("name")
-                        .commit();
-
+                replaceFragment(fragmentClass, backstackName);
             }
         });
+    }
 
-        Button btnYear = findViewById(R.id.yearBtn);
-        btnYear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Handle grade button click
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                fragmentManager.beginTransaction()
-                        .replace(R.id.yearFragment, YearFragment.class, null)
-                        .setReorderingAllowed(true)
-                        .addToBackStack("name")
-                        .commit();
-
-            }
-        });
-
-    }}
+    private void replaceFragment(Class<? extends Fragment> fragmentClass, String backstackName) {
+        try {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.gradeFragment, fragmentClass.newInstance());
+            fragmentTransaction.setReorderingAllowed(true);
+            fragmentTransaction.addToBackStack(backstackName);
+            fragmentTransaction.commit();
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
